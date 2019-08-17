573ff74a9f42e19ee03a7c2012dab1a2d09ed427
Fran Mira <Fran Mira@DESKTOP-P06S23T.Home>
2019-08-18 01:03:48 +0200

    Added all functions

--- jdbc/src/main/java/es/eoi/jdbc/app/GestionInstituto.java
+++ jdbc/src/main/java/es/eoi/jdbc/app/GestionInstituto.java
@@ -1,5 +1,10 @@
 package es.eoi.jdbc.app;
 
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import es.eoi.jdbc.entity.Alumno;
 import es.eoi.jdbc.service.AlumnoService;
 import es.eoi.jdbc.service.AlumnoServiceImpl;
@@ -8,17 +13,190 @@ public class GestionInstituto {
 	public static AlumnoService myService;
 
 	public static void main(String[] args) {
-		
+
 		myService = new AlumnoServiceImpl();
-		
-//		Alumno alumnoFound = myService.findByDni("48774753X");
-//		
-//		System.out.println("Alumno encontrado");
-//		System.out.println("Nombre: " + alumnoFound.getNombre());
-		
+		printMenu();
 	}
-	
+
 	public static void printMenu() {
+		int option = 0;
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("GESTION INSTITUTO V1");
+		System.out.println("-------------------------------");
+		System.out.println("1 –Listado Alumnos");
+		System.out.println("2 –Busca Alumno (DNI)");
+		System.out.println("3 –Crear Alumno");
+		System.out.println("4 –Modificar Alumno");
+		System.out.println("5 –Eliminar Alumno");
+		System.out.println("0 –SALIR");
+		System.out.println("-------------------------------");
+
+		if (scan.hasNextInt()) {
+			option = scan.nextInt();
+		} else {
+			System.out.println("No se ha introducido una opcion correcta");
+			printMenu();
+		}
+
+		switch (option) {
+		case 1:
+			listAlumnos();
+			break;
+		case 2:
+			searchAlumno();
+			break;
+		case 3:
+			createAlumno();
+			break;
+		case 4:
+			updateAlumno();
+			break;
+		case 5:
+			deleteAlumno();
+			break;
+		case 0:
+			System.out.println("Saliendo del programa...");
+			return;
+		default:
+			break;
+		}
+		printMenu();
+	}
+
+	private static void searchAlumno() {
+		String parteNumericaDni = "";
+		String letraDni = "";
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Introduce el DNI del alumno a buscar");
+		String dniIntroducido = scan.nextLine();
+		if (validarDNI(dniIntroducido)) {
+			myService.findByDni(dniIntroducido);
+		} else {
+			System.out.println("El DNI que ha introducido es erroneo");
+			searchAlumno();
+		}
+	}
+
+	public static boolean validarDNI(String dni) {
+
+		boolean correcto = false;
+
+		Pattern pattern = Pattern.compile("(\\d{1,8})([TRWAGMYFPDXBNJZSQVHLCKEtrwagmyfpdxbnjzsqvhlcke])");
+		Matcher matcher = pattern.matcher(dni);
+
+		if (matcher.matches()) {
+
+			String letra = matcher.group(2);
+			String letras = "TRWAGMYFPDXBNJZSQVHLCKE";
+			int index = Integer.parseInt(matcher.group(1));
+
+			index = index % 23;
+
+			String reference = letras.substring(index, index + 1);
+
+			if (reference.equalsIgnoreCase(letra)) {
+				correcto = true;
+			} else {
+				correcto = false;
+			}
+		} else {
+			correcto = false;
+		}
+
+		return correcto;
+
+	}
+	private static void listAlumnos() {
+		List<Alumno> listAlumno = myService.findAll();
 		
+		System.out.println("Alumnos encontrados:");
+		
+		for (Alumno alumno : listAlumno) {
+			System.out.println("DNI: ".concat(alumno.getDni()));
+			System.out.println("Nombre: ".concat(alumno.getNombre()));
+			System.out.println("Apellidos: ".concat(alumno.getApellido()));
+			System.out.println("Edad: ".concat(String.valueOf(alumno.getEdad())));
+			System.out.println();
+		}
+	}
+	private static void createAlumno() {
+		Scanner scan = new Scanner(System.in);
+		int edad = 0;
+
+		System.out.println("Introduce el DNI del alumno:");
+		String dni = scan.nextLine();
+		System.out.println(dni);
+
+		if (!validarDNI(dni)) {
+			System.out.println("El DNII que ha introducido es erroneo");
+			createAlumno();
+		}
+
+		System.out.println("Introduce el nombre del alumno:");
+		String nombre = scan.nextLine();
+
+		scan.reset();
+
+		System.out.println("Introduce el apellido del alumno:");
+		String apellido = scan.nextLine();
+
+		do {
+			System.out.println("Introduce la edad del alumno:");
+			if (scan.hasNextInt()) {
+				edad = scan.nextInt();
+			} else {
+				System.out.println("Ha introducido un valor erroneo para la edad");
+			}
+		} while (edad <= 0);
+
+		Alumno alumno = new Alumno(nombre, apellido, edad, dni);
+
+		if (myService.create(alumno)) {
+			System.out.println("Alumno registrado correctamente");
+		} else {
+			System.out.println("ERROR - No se ha registrado el alumno correctamente");
+		}
+	}
+
+	private static void updateAlumno() {
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Introduce el DNI del alumno a modificar");
+		String dni = scan.nextLine();
+
+		if (!validarDNI(dni)) {
+			System.out.println("El DNI introducido no es valido");
+			updateAlumno();
+		}
+
+		System.out.println("Introduce el nombre que sera registrado");
+		String nombre = scan.nextLine();
+
+		System.out.println("Introduce los apellidos que seran registrados");
+		String apellidos = scan.nextLine();
+
+		if (myService.update(dni, nombre, apellidos)) {
+			System.out.println("Alumno modificado correctamente");
+		} else {
+			System.out.println("No se ha modificado correctamente al alumno");
+		}
+
+	}
+
+	private static void deleteAlumno() {
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Introduce el DNI del alumno a modificar");
+		String dni = scan.nextLine();
+
+		if (!validarDNI(dni)) {
+			System.out.println("El DNI introducido no es valido");
+			deleteAlumno();
+		}
+		myService.delete(dni);
 	}
 }
--- jdbc/src/main/java/es/eoi/jdbc/repository/AlumnoRepositoryImpl.java
+++ jdbc/src/main/java/es/eoi/jdbc/repository/AlumnoRepositoryImpl.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import es.eoi.jdbc.entity.Alumno;
@@ -22,7 +24,8 @@ public class AlumnoRepositoryImpl implements AlumnoRepository {
 			System.out.println("Conectado a ".concat(url));
 
 		} catch (Exception e) {
-			System.out.println("No se ha realizado correctamente la accion");
+			System.err.println("No se ha realizado correctamente la accion");
+			System.err.println(e.getMessage());
 		}
 
 		return conexion;
@@ -32,54 +35,180 @@ public class AlumnoRepositoryImpl implements AlumnoRepository {
 		String dniFound = null;
 		String nombre = null;
 		String apellido = null;
+		int edad = 0;
 
 		StringBuilder sb = new StringBuilder();
 		sb.append("SELECT * ");
 		sb.append("FROM ALUMNO ");
 		sb.append("WHERE DNI = ? ");
 
-		// ResultSet rs = null;
 		try {
-
+			
 			Connection conn = openConnection();
 
 			PreparedStatement pst = conn.prepareStatement(sb.toString());
-			pst.setString(1, "48774753X");
+			pst.setString(1, dni);
 
 			ResultSet rs = pst.executeQuery();
-			
+
 			while (rs.next()) {
 				dniFound = rs.getString("Dni");
 				nombre = rs.getString("Nombre");
 				apellido = rs.getString("Apellidos");
+				edad = rs.getInt("Edad");
 			}
-
+			
+			conn.close();
+			
 		} catch (Exception e) {
-			System.out.println("Algo ha ido mal");
+			System.err.println("No se ha realizado correctamente la accion");
+			System.err.println(e.getMessage());
 		}
 
-		Alumno newAlumno = new Alumno(nombre, apellido, 24, dniFound);
+		Alumno newAlumno = new Alumno(nombre, apellido, edad, dniFound);
 		System.out.println("DEBUG Nombre:" + newAlumno.getNombre() + " edad: " + newAlumno.getEdad());
 		return newAlumno;
 	}
 
 	public List<Alumno> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+
+		String dniFound = null;
+		String nombre = null;
+		String apellido = null;
+		int edad = 0;
+
+		List<Alumno> selectedAlumnos = null;
+
+		StringBuilder sb = new StringBuilder();
+		sb.append("SELECT * ");
+		sb.append("FROM ALUMNO ");
+		sb.append("WHERE DNI = ? ");
+
+		try {
+			selectedAlumnos = new ArrayList<Alumno>();
+			Connection conn = openConnection();
+
+			Statement st = conn.createStatement();
+
+			ResultSet rs = st.executeQuery("SELECT * FROM ALUMNO");
+
+			while (rs.next()) {
+				dniFound = rs.getString("Dni");
+				nombre = rs.getString("Nombre");
+				apellido = rs.getString("Apellidos");
+				edad = rs.getInt("Edad");
+
+				Alumno newAlumno = new Alumno(nombre, apellido, edad, dniFound);
+				selectedAlumnos.add(newAlumno);
+			}
+			conn.close();
+		} catch (Exception e) {
+			System.err.println("No se ha realizado correctamente la accion");
+			System.err.println(e.getMessage());
+		}
+
+		return selectedAlumnos;
 	}
 
 	public boolean create(Alumno alumno) {
-		// TODO Auto-generated method stub
+		int resultQuery = 0;
+
+		StringBuilder sb = new StringBuilder();
+		sb.append("INSERT INTO ");
+		sb.append("ALUMNO ");
+		sb.append("VALUES( ");
+		sb.append(" ? ,");
+		sb.append(" ? ,");
+		sb.append(" ? ,");
+		sb.append(" ? );");
+
+		try {
+
+			Connection conn = openConnection();
+
+			PreparedStatement pst = conn.prepareStatement(sb.toString());
+			pst.setString(1, alumno.getDni());
+			pst.setString(2, alumno.getNombre());
+			pst.setString(3, alumno.getApellido());
+			pst.setInt(4, alumno.getEdad());
+
+			resultQuery = pst.executeUpdate();
+			conn.close();
+		} catch (Exception e) {
+			System.err.println("No se ha realizado correctamente la accion");
+			System.err.println(e.getMessage());
+		}
+
+		if (resultQuery > 0) {
+			return true;
+		}
 		return false;
 	}
 
 	public boolean delete(String dni) {
-		// TODO Auto-generated method stub
+
+		int resultQuery = 0;
+
+		StringBuilder sb = new StringBuilder();
+		sb.append("DELETE ");
+		sb.append("FROM ALUMNO ");
+		sb.append("WHERE DNI = ? ");
+
+		try {
+			Connection conn = openConnection();
+
+			PreparedStatement pst = conn.prepareStatement(sb.toString());
+			pst.setString(1, dni);
+
+			resultQuery = pst.executeUpdate();
+
+			conn.close();
+		} catch (Exception e) {
+			System.err.println("No se ha realizado correctamente la accion");
+			System.err.println(e.getMessage());
+		}
+
+		if (resultQuery >= 1) {
+			System.out.println("Alumno borrado correctamente");
+			return true;
+		}
 		return false;
+
 	}
 
 	public boolean update(String dni, String nombre, String apellidos) {
-		// TODO Auto-generated method stub
+		
+		int resultQuery = 0;
+
+		StringBuilder sb = new StringBuilder();
+		sb.append("UPDATE ALUMNO ");
+		sb.append("SET ");
+		sb.append("Nombre = ? ,");
+		sb.append("Apellidos = ? ");
+		sb.append("WHERE DNI = ? ;");
+
+		try {
+			Connection conn = openConnection();
+
+			PreparedStatement pst = conn.prepareStatement(sb.toString());
+			pst.setString(1, nombre);
+			pst.setString(2, apellidos);
+			pst.setString(3, dni);
+
+			resultQuery = pst.executeUpdate();
+
+			conn.close();
+		} catch (Exception e) {
+			System.err.println("No se ha realizado correctamente la accion");
+			System.err.println(e.getMessage());
+		}
+
+		if (resultQuery > 0) {
+			System.out.println("Alumno modificado correctamente");
+			return true;
+		}
+		System.out.println("No se ha podido modificar al alumno correctamente");
+
 		return false;
 	}
 
